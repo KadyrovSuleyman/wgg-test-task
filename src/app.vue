603d0c5eb0d07ba2405cad89429adcc40d45a565
Кадыров Сулейман <kadyrov.suleyman@yandex.ru>
2022-04-13 15:10:33 +0300

    connect to VK

--- src/app.vue
+++ src/app.vue
@@ -1,5 +1,35 @@
 <script setup lang="ts">
 
+let VK: any;
+
+const script = document.createElement('script');
+script.src = 'https://vk.com/js/api/openapi.js?169';
+document.head.append(script);
+
+const get = () => {
+  VK.Auth.login((data) => {
+    VK.Api.call('friends.get', {
+      user_id: data.session.user.id, v: '5.134', fields: 'nickname, photo_50', order: 'random',
+    }, (r) => {
+      console.log(r);
+    });
+  });
+};
+
+script.onload = () => {
+  VK = window.VK;
+
+  Promise.resolve()
+    .then(() => {
+      VK.init({
+        apiId: 8136088,
+      });
+    })
+    .then(() => {
+      // get();
+    });
+};
+
 </script>
 
 <template>
